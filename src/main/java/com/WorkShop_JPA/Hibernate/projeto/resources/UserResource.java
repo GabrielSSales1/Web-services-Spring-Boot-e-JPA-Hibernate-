f0e74035af4fd809a8d6fcbea83addb3672e31c5
unknown <gss.devtech@gmail.com>
2026-07-27 11:31:21 -0300

    User entity and Resource

---
+++ src/main/java/com/WorkShop_JPA/Hibernate/projeto/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.WorkShop_JPA.Hibernate.projeto.resources;
+
+import com.WorkShop_JPA.Hibernate.projeto.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L,"Maria", "maria@gmail.com","999999","12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
